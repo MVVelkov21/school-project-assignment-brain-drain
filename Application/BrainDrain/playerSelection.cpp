bdc69ab60f1bbc08506464eb68a71f21e9e223f1
SKDimitrov22 <SKDimitrov22@codingburgas.bg>
2024-04-21 16:03:39 +0300

    Display playable characters

--- Application/BrainDrain/playerSelection.cpp
+++ Application/BrainDrain/playerSelection.cpp
@@ -3,12 +3,17 @@
 void levelBuilder()
 {
 	Texture2D background = LoadTexture("../assets/settingsMenu/settingsMenuBg.png");
-	
+	Texture2D girl = LoadTexture("../assets/settingsMenu/girlPlayer.png");
+	Texture2D boy = LoadTexture("../assets/settingsMenu/boyPlayer.png");
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 		DrawTexture(background, 0, 0, WHITE);
+		DrawTexture(boy, (GetScreenWidth() / 2) - 200, (GetScreenHeight() / 2) - 20, WHITE);
+		DrawTexture(girl, (GetScreenWidth() / 2), (GetScreenHeight() / 2) - 7, WHITE);
+
 		EndDrawing();
 	}
 }
