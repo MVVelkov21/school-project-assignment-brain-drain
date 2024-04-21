141cd0b091868de1a467a8d01f29c1de04d4ca3e
IDDimov21 <IDDimov21@codingburgas.bg>
2024-04-21 23:00:38 +0300

    Add products to each solution

--- Application/BrainDrain/madScientists.cpp
+++ Application/BrainDrain/madScientists.cpp
@@ -30,11 +30,12 @@ void madScientists::resetElement(Rectangle& el1, Rectangle& final, bool& drag1, 
     }
 }
 
-void madScientists::fillTube(Rectangle& el1, Rectangle& final, bool& drag1, int& c, int cordTX, int cordTY) {
+void madScientists::fillTube(Rectangle& el1, Rectangle& final, bool& drag1, bool& flag ,int& c, int cordTX, int cordTY) {
     if (CheckCollisionRecs(el1, final) && drag1 == false) {
         c++;
         el1.x = cordTX;
         el1.y = cordTY;
+        flag = true;
     }
 }
 
@@ -46,10 +47,11 @@ void madScientists::levelBuilder() {
     tubeFull = LoadTexture("../assets/madScientists/tubeStage3.png");
     H = LoadTexture("../assets/madScientists/H.png");
     C = LoadTexture("../assets/madScientists/C.png");
-    N = LoadTexture("../assets/madScientists/N.png");
+    S = LoadTexture("../assets/madScientists/S.png");
     O = LoadTexture("../assets/madScientists/O.png");
     x1 = 170, y1 = 80, x2 = 280, y2 = 80, x3 = 390, y3 = 80, x4 = 500, y4 = 80;
     isDraggedEl1 = false, isDraggedEl2 = false, isDraggedEl3 = false, isDraggedEl4 = false;
+    el1Tube = false, el2Tube = false, el3Tube = false, el4Tube = false;
     fillCounter = 0;
     tubePos = { 200, 250 };
     tubeHitBox = { 220, 267, 120, 120 };
@@ -66,10 +68,10 @@ void madScientists::levelBuilder() {
         resetElement(Element2, tubeHitBox, isDraggedEl2, x2, y2, 280, 80);
         resetElement(Element3, tubeHitBox, isDraggedEl3, x3, y3, 390, 80);
         resetElement(Element4, tubeHitBox, isDraggedEl4, x4, y4, 500, 80);
-        fillTube(Element1, tubeHitBox, isDraggedEl1, fillCounter, 170, 80);
-        fillTube(Element2, tubeHitBox, isDraggedEl2, fillCounter, 280, 80);
-        fillTube(Element3, tubeHitBox, isDraggedEl3, fillCounter, 390, 80);
-        fillTube(Element4, tubeHitBox, isDraggedEl4, fillCounter, 500, 80);
+        fillTube(Element1, tubeHitBox, isDraggedEl1, el1Tube,fillCounter, 170, 80);
+        fillTube(Element2, tubeHitBox, isDraggedEl2, el2Tube,fillCounter, 280, 80);
+        fillTube(Element3, tubeHitBox, isDraggedEl3, el3Tube,fillCounter, 390, 80);
+        fillTube(Element4, tubeHitBox, isDraggedEl4, el4Tube,fillCounter, 500, 80);
         dragDrop(Element1, isDraggedEl1, x1, y1);
         dragDrop(Element2, isDraggedEl2, x2, y2);
         dragDrop(Element3, isDraggedEl3, x3, y3);
@@ -93,7 +95,7 @@ void madScientists::levelBuilder() {
 
         DrawTexture(H, x1, y1, RAYWHITE);
         DrawTexture(C, x2, y2, RAYWHITE);
-        DrawTexture(N, x3, y3, RAYWHITE);
+        DrawTexture(S, x3, y3, RAYWHITE);
         DrawTexture(O, x4, y4, RAYWHITE);
 
         if (fillCounter == 0)
@@ -103,8 +105,25 @@ void madScientists::levelBuilder() {
         }
         else if (fillCounter == 2) {
             DrawTextureV(tubeFull, tubePos, RAYWHITE);
+            if (el1Tube && el4Tube) {
+                DrawText("H2O", 200, 250, 24, BLACK); //Wasser
+            }
+            else if (el1Tube && el2Tube) {
+                DrawText("CH4", 200, 250, 24, BLACK); //Methane
+            }
+            else if (el1Tube && el3Tube) {
+                DrawText("H2S", 200, 250, 24, BLACK); //Hydrogen sulfide
+            }
+            else if (el2Tube && el4Tube) {
+                DrawText("CO2", 200, 250, 24, BLACK); //Carbon dioxide
+            }
+            else if (el3Tube && el4Tube) {
+                DrawText("SO2", 200, 250, 24, BLACK); //Sulfur dioxide
+            }
+            else {
+                DrawText("BOOM", 200, 250, 24, BLACK);
+            }
         }
-
         EndDrawing();
     }
     UnloadTexture(background);
--- Application/BrainDrainLib/madScientists.h
+++ Application/BrainDrainLib/madScientists.h
@@ -4,15 +4,16 @@
 class madScientists {
 	private:
 		bool isDraggedEl1, isDraggedEl2, isDraggedEl3, isDraggedEl4;
+		bool el1Tube, el2Tube, el3Tube, el4Tube;
 		int fillCounter;
 		float x1, y1, x2, y2, x3, y3, x4, y4;
 		Texture2D tube, tubeHalf, tubeFull;
-		Texture2D background, H, C, N, O;
+		Texture2D background, H, C, S, O;
 		Rectangle Element1, Element2, Element3, Element4, tubeHitBox;
 		Vector2 tubePos;
 	public: 
 		void levelBuilder();
 		void dragDrop(Rectangle& rect, bool& isDragged, float& CordX, float& CordY);
 		void resetElement(Rectangle& el1, Rectangle & final, bool& drag1, float& CordX, float& CordY, int cordTX, int cordTY);
-		void fillTube(Rectangle& el1, Rectangle & final, bool& drag1, int& c, int cordTX, int cordTY);
+		void fillTube(Rectangle& el1, Rectangle & final, bool& drag1, bool& flag, int& c, int cordTX, int cordTY);
 };
