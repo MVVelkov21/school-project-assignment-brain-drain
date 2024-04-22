4437d8d1617aa18ad0c1e2e2accd12307b9ced25
IZDandarinov21 <IZDandarinov21@codingburgas.bg>
2024-04-22 20:57:40 +0300

    Add Native Test project

---
+++ testFunctions/testFunctions.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testFunctions
+{
+	TEST_CLASS(testFunctions)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
