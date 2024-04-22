3ba82aa668d6b9f7217b7d0bafb2aba549b41dc3
IZDandarinov21 <IZDandarinov21@codingburgas.bg>
2024-04-22 21:26:13 +0300

    Test all functions

--- testFunctions/testFunctions.cpp
+++ testFunctions/testFunctions.cpp
@@ -59,9 +59,9 @@ namespace testFunctions
         }
         TEST_METHOD(reworkedResetElementTest)
         {
-           
+
             bool isCollision1 = false;
-            bool isCollision2 = true; 
+            bool isCollision2 = true;
             bool isDragged1 = false;
             bool isDragged2 = false;
             int el1X = 200;
@@ -71,40 +71,40 @@ namespace testFunctions
 
             reworkedResetElement(isCollision1, isCollision2, isDragged1, isDragged2, el1X, el1Y, el2X, el2Y);
 
-            Assert::AreEqual(170, el1X); 
-            Assert::AreEqual(80, el1Y); 
-            Assert::AreEqual(300, el2X); 
-            Assert::AreEqual(150, el2Y); 
+            Assert::AreEqual(170, el1X);
+            Assert::AreEqual(80, el1Y);
+            Assert::AreEqual(300, el2X);
+            Assert::AreEqual(150, el2Y);
         }
         TEST_METHOD(reworkedFillTubeTest)
         {
-            
-            bool isCollision1 = true; 
+
+            bool isCollision1 = true;
             bool isCollision2 = false;
             bool isDragged1 = false;
-            bool isDragged2 = true; 
-            int counter = 0; 
+            bool isDragged2 = true;
+            int counter = 0;
 
             reworkedFillTube(isCollision1, isCollision2, isDragged1, isDragged2, counter);
 
-            Assert::AreEqual(1, counter); 
+            Assert::AreEqual(1, counter);
         }
     };
     TEST_CLASS(TestSuperMathsSimpleFunctions)
     {
     public:
 
-        
+
         TEST_METHOD(reworkedLoadProblemsAndSymbolsTest)
         {
-            
+
             vector<string> problems;
             vector<vector<string>> symbolsByProblem;
 
             reworkedLoadProblemsAndSymbols(problems, symbolsByProblem);
 
-            Assert::AreEqual(static_cast<size_t>(3), problems.size()); 
-            Assert::AreEqual(static_cast<size_t>(3), symbolsByProblem.size()); 
+            Assert::AreEqual(static_cast<size_t>(3), problems.size());
+            Assert::AreEqual(static_cast<size_t>(3), symbolsByProblem.size());
         }
     };
 }
