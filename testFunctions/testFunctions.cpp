2423c37c31611f6f420104452475ee07bcfd16c8
IZDandarinov21 <IZDandarinov21@codingburgas.bg>
2024-04-22 21:07:53 +0300

    Add test case for PrintMessage function

--- testFunctions/testFunctions.cpp
+++ testFunctions/testFunctions.cpp
@@ -1,16 +1,33 @@
 #include "pch.h"
+#include <fstream>
+#include <string>
+#include <vector>
 #include "CppUnitTest.h"
-
+#include "reworkedFunctions.h"
+#include "../BrainDrainLib/precompiledHeader.h"
+#include "../raylib/include/raylib.h"
+#include "../BrainDrainLib/superMaths.h"
+#include "../BrainDrainLib/riddleRoute.h"
+#include "../BrainDrainLib/madScientists.h"
+#include "../BrainDrainLib/playerSelection.h"
+#include "../BrainDrainLib/gameSelection.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace testFunctions
 {
-	TEST_CLASS(testFunctions)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-		}
-	};
+    TEST_CLASS(testrReworkedPrintMessageTest)
+    {
+    public:
+
+        TEST_METHOD(reworkedPrintMessageTest)
+        {
+
+            const char* message = "This is a test message.";
+
+
+            reworkedPrintMessage(message);
+
+
+        }
+    };
 }
